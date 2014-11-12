7634b411fa21d689422a6b932c7931ba710933b2
Nikola Derikonjic <nikola@pregmatch.org>
2014-11-12 09:08:54 +0100

    Post commnet

---
+++ CommentModel.py
@@ -0,0 +1,37 @@
+from DB import *
+import time
+class CommentModel:
+
+    def __init__(self):
+        self.cryptkey = appconfig.CRYPT_KEY
+
+
+    def comments(self):
+
+        db = DB()
+        sql = '''SELECT * FROM Comment'''
+
+        query = db.query(sql)
+        comments = query.fetchall()
+
+        db.close()
+
+        if comments:
+            return comments
+        return False
+
+    def addcomment(self, comment, email, nick, post):
+
+        db = DB()
+        sql = '''INSERT INTO Comment (Id, Comment, Post, Nick, Email, DateCreated) VALUES (NULL, %s, %s, %s, %s, %s)'''
+
+        q = db.query(sql, (comment,post,nick, email, time.strftime('%Y-%m-%d %H:%M:%S')) )
+        db.conn.commit()
+        db.close()
+
+
+        return  q.lastrowid;
+        #todo return id or something
+
+
+
