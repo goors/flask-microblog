b6d1d4f6cb804218bce6ec7f7b69af46a22a69bf
Nikola Derikonjic <nikola@pregmatch.org>
2014-11-16 11:20:36 +0100

    COmpress

--- blog.py
+++ blog.py
@@ -5,7 +5,7 @@ from controllers import static
 from controllers import admin
 from controllers import fe
 from appconfig import *
-
+from flask.ext.compress import Compress
 
 account_api = account.account_api
 static_api = static.static_api
@@ -13,6 +13,7 @@ admin_api = admin.admin_api
 fe = fe.fe
 
 app = Flask(__name__)
+Compress(app)
 app.register_blueprint(account_api)
 app.register_blueprint(static_api)
 app.register_blueprint(admin_api)
